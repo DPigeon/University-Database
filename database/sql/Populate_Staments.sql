d2a6c1854160b006f5e0b6af0583d91cf576fcd3
LiamBlount <50602230+LiamBlount@users.noreply.github.com>
2019-07-15 19:41:41 -0400

    Create Populate_Staments.sql

---
+++ database/sql/Populate_Staments.sql
@@ -0,0 +1,98 @@
+INSERT INTO Instructor VALUES( 1, 'Nicholas Larsen');
+INSERT INTO Instructor VALUES( 2, 'Ernest Steig');
+INSERT INTO Instructor VALUES( 3, 'Melissa Roberts');
+INSERT INTO Instructor VALUES( 4, 'Lisa Cranterson');
+INSERT INTO Instructor VALUES( 5, 'Jake Ralph');
+INSERT INTO Instructor VALUES( 6, 'Linda Torrents');
+INSERT INTO Instructor VALUES( 7, 'Claire Devons');
+INSERT INTO Instructor VALUES( 8, 'Isaac Harrison');
+INSERT INTO Instructor VALUES( 9, 'Jeremy Kudo');
+INSERT INTO Instructor VALUES( 10, 'Jessica Stevenson');
+
+INSERT INTO Work VALUES( 'Physics', 1);
+INSERT INTO Work VALUES( 'Mathematics', 2);
+INSERT INTO Work VALUES( 'Biology', 3);
+INSERT INTO Work VALUES( 'Engineering', 4);
+INSERT INTO Work VALUES( 'Gender Studies', 5);
+INSERT INTO Work VALUES( 'Anthropology', 6);
+INSERT INTO Work VALUES( 'French', 7);
+INSERT INTO Work VALUES( 'Jewish Studies', 8);
+INSERT INTO Work VALUES( 'History', 9);
+INSERT INTO Work VALUES( 'Computer Science', 10);
+
+INSERT INTO Teach VALUES( 1, 1);
+INSERT INTO Teach VALUES( 2, 2);
+INSERT INTO Teach VALUES( 3, 3);
+INSERT INTO Teach VALUES( 4, 4);
+INSERT INTO Teach VALUES( 5, 5);
+INSERT INTO Teach VALUES( 6, 6);
+INSERT INTO Teach VALUES( 7, 7);
+INSERT INTO Teach VALUES( 8, 8);
+INSERT INTO Teach VALUES( 9, 9);
+INSERT INTO Teach VALUES( 10, 10);
+
+INSERT INTO Section VALUES( 1, 'Fall',2019);
+INSERT INTO Section VALUES( 2, 'Winter',2020);
+INSERT INTO Section VALUES( 3, 'Summer',2019);
+INSERT INTO Section VALUES( 4, 'Fall',2019);
+INSERT INTO Section VALUES( 5, 'Winter',2020);
+INSERT INTO Section VALUES( 6, 'Summer',2019);
+INSERT INTO Section VALUES( 7, 'Fall',2019);
+INSERT INTO Section VALUES( 8, 'Winter',2020);
+INSERT INTO Section VALUES( 9, 'Summer',2019);
+INSERT INTO Section VALUES( 10, 'Fall',2019);
+
+INSERT INTO EnrolledIn VALUES( 1, 2);
+INSERT INTO EnrolledIn VALUES( 2, 3);
+INSERT INTO EnrolledIn VALUES( 3, 4);
+INSERT INTO EnrolledIn VALUES( 4, 5);
+INSERT INTO EnrolledIn VALUES( 5, 6);
+INSERT INTO EnrolledIn VALUES( 6, 7);
+INSERT INTO EnrolledIn VALUES( 7, 8);
+INSERT INTO EnrolledIn VALUES( 8, 9);
+INSERT INTO EnrolledIn VALUES( 9, 10);
+INSERT INTO EnrolledIn VALUES( 10, 1);
+
+INSERT INTO Class VALUES(1, 08:45:00, 737,'A Building',150);
+INSERT INTO Class VALUES(2, 09:30:00, 434,'B Building',125);
+INSERT INTO Class VALUES(3, 10:00:00, 736,'C Building',100);
+INSERT INTO Class VALUES(4, 11:45:00, 907,'D Building',145);
+INSERT INTO Class VALUES(5, 12:45:00, 675,'E Building',190);
+INSERT INTO Class VALUES(6, 10:30:00, 234,'F Building',250);
+INSERT INTO Class VALUES(7, 11:00:00, 943,'G Building',203);
+INSERT INTO Class VALUES(8, 10:45:00, 390,'H Building',109);
+INSERT INTO Class VALUES(9, 11:15:00, 444,'I Building',121);
+INSERT INTO Class VALUES(10, 12:30:00, 656,'J Building',153);
+
+INSERT INTO Class VALUES(08:45:00, 'Monday',1,09:45:00);
+INSERT INTO Class VALUES(09:30:00, 'Wednesday',2,10:45:00);
+INSERT INTO Class VALUES(10:00:00, 'Tuesday',3,11:45:00);
+INSERT INTO Class VALUES(11:45:00, 'Thursday',4,12:45:00);
+INSERT INTO Class VALUES(12:45:00, 'Friday',5,13:45:00);
+INSERT INTO Class VALUES(10:30:00, 'Monday',6,12:45:00);
+INSERT INTO Class VALUES(11:00:00, 'Monday',7,12:45:00);
+INSERT INTO Class VALUES(10:45:00, 'Wednesday',8,11:45:00);
+INSERT INTO Class VALUES(11:15:00, 'Thursday,9,13:45:00);
+INSERT INTO Class VALUES(12:30:00, 'Friday',10,14:45:00);
+
+INSERT INTO Department VALUES('Computer Science', 1, 'A Building');
+INSERT INTO Department VALUES('Mathematics', 2, 'B Building');
+INSERT INTO Department VALUES('Biology', 3, 'C Building');
+INSERT INTO Department VALUES('Physics', 4, 'D Building');
+INSERT INTO Department VALUES('Engineering', 5, 'E Building');
+INSERT INTO Department VALUES('French', 6, 'F Building');
+INSERT INTO Department VALUES('Gender Studies', 7, 'G Building');
+INSERT INTO Department VALUES('History', 8, 'H Building');
+INSERT INTO Department VALUES('Anthropology', 9, 'I Building');
+INSERT INTO Department VALUES('Jewish Studies', 10, 'J Building');
+
+INSERT INTO Course VALUES( 1,'Operating Systems' ,'Computer Science');
+INSERT INTO Course VALUES( 2,'Mechanics' ,'Physics');
+INSERT INTO Course VALUES( 3,'Advanced French Language' ,'French');
+INSERT INTO Course VALUES( 4,'Sexuality' ,'Gender Studies');
+INSERT INTO Course VALUES( 5,'MicroBiology' ,'Biology');
+INSERT INTO Course VALUES( 6,'Algebra' ,'Mathematics');
+INSERT INTO Course VALUES( 7,'The Roman Empire' ,'History');
+INSERT INTO Course VALUES( 8,'Flammable Materials' ,'Engineering');
+INSERT INTO Course VALUES( 9,'Evolution' ,'Anthropology');
+INSERT INTO Course VALUES( 10,'Talmudic Studies' ,'Jewish Studies');
