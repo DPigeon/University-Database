dd8662994d22afec53eccdbe628ca6eb19c75d73
DPigeon <D_Pig@encs.concordia.ca>
2019-07-15 12:09:30 -0400

    SQL Queries, Diagram & Tables
    Co-authored-by: mhege <hegedusmarc@gmail.com>
    Co-authored-by: iana7 <yana_cr7@live.com>

--- database/sql/queries.sql
+++ database/sql/queries.sql
@@ -1,38 +1,72 @@
 /* All queries needed for the warmUp project */
 
-/* Query 1 */
-SELECT ID, FirstName, LastName
-FROM Student, Course
-WHERE CName='Database' AND (grade='A' OR grade='A+');
+/* 
+Query 1 */SELECT Student.STID, FirstName, LastName
+FROM Student INNER JOIN Course ON Course.CID = Student.CID,
+	Student INNER JOIN EnrolledIn ON Student.STID = EnrolledIn.STID
+WHERE CName='COMP352' AND (Grade='A' OR Grade='A+');
 
-/* Query 2 */
-SELECT ID, FirstName, LastName, count(PName)
-FROM belong, Student, Program
-GROUP BY Student.ID
-Having count(PName)>1;
+/* 
 
-/* Query 3 */
-SELECT Instructor.name, count(*)
-FROM Section, Course, Instructor
-GROUP BY Instructor_name
-Having CName='COMP352' AND Semester='Fall' AND year=2018 AND count(*)=1;
+Query 2 */SELECT Student.STID, FirstName, LastName, COUNT(Program.PName)
+FROM Student INNER JOIN Program ON Student.PName = Program.PName
+GROUP BY Student.STID
+HAVING COUNT(Program.PName)>1;
 
-/* Query 4 */
-SELECT distinct P.PName, P.TotalCredit
+/* 
+
+Query 3 */SELECT Instructor.name, count(Instructor.Name)
+FROM Section INNER JOIN Course ON Section.CID = Course.CID,
+	Section INNER JOIN Instructor ON Instructor.IID = Section.IID
+GROUP BY Instructor.name
+Having CName='COMP352' AND Semester='Fall' AND Year=2018 AND count(Instructor.Name)=1;
+
+/* 
+
+Query 4 */SELECT DISTINCT P.PName, P.TotalCredit
 FROM Program P, department
-where dName='Computer Science';
+WHERE DName='ComputerScience';
 
-/* Query 5 */
-SELECT U.UName, U.UID
+/*
+
+Query 5 */SELECT U.UName, U.UID
 FROM Undergraduate as U
-where advisor=NULL;
+WHERE advisor=NULL;
 
-/* Query 6 */
+/* 
 
-/* Query 7 */
+Query 6 */SELECT STID, FirstName, LastName, AssignmentMarking
+FROM TeachingAssistant INNER JOIN Section ON TeachingAssistant.STID = Section.STID,
+	Section INNER JOIN Course ON Section.CID = Course.CID
+WHERE CName='COMP353' AND Semester='Summer' AND Year=2019;
 
-/* Query 8 */
+/* 
 
-/* Query 9 */
+Query 7 */SELECT Instructor.Name
+FROM Graduate INNER JOIN Instructors ON Graduate.SupervisorID = Instructor.IID,
+	Instructor INNER JOIN Department ON Instructor.DName = Department.DName
+GROUP BY Instructor.Name
+HAVING DName = 'ComputerScience' AND COUNT(*) >= 20;/* 
 
-/* Query 10 */
+Query 8 */SELECT CName, SID, Class.Building, Class.Number, Class.Capacity, Instructor.Name, COUNT(STID)
+FROM Department INNER JOIN Course ON Department.DName = Course.DName,
+	Department INNER JOIN Instructor ON Department.DName = Instructor.DName,
+	Instructor INNER JOIN Section ON Instructor.IID = Section.IID,
+	Section INNER JOIN Class ON Class.CLID = Section.SID,
+	Timeslot INNER JOIN Class ON Class.CLID = Timeslot.CLID,
+	Student INNER JOIN Section ON Section.STID = Student.STID
+WHERE DName = 'ComputerScience';
+
+/* 
+
+Query 9 */SELECT DName, COUNT(CID)
+FROM Department INNER JOIN Course ON Department.DName = Course.DName
+GROUP BY DName;
+
+/* 
+	  
+Query 10 */SELECT PName, COUNT(STID)
+FROM Program INNER JOIN Student ON Student.STID = Program.STID
+GROUP BY PName;
+
+/*
--- database/sql/tables.sql
+++ database/sql/tables.sql
@@ -1,17 +1,25 @@
-/* - Students: all attributes from table+ID renamed to STID+many to one relation ‘’Belong’’ i.e added primary key Pname from program
-   - I removed the M to 1  relation Study to department because a student can study in many departments and this link is encapsulated in the ‘’Belong’’
-   - **Edit M to M not M to 1 added Belong table removed advisor from student** 
-*/
+/* Tables */
 
-/* Note from David: shouldn't we define our PRIMARY KEY and FOREIGN KEY ? */
-CREATE TABLE Students
+CREATE TABLE Student
 (
     STID INT,
     Credit INT,
     FirstName CHAR (30),
     LastName CHAR (30),
     GPA FLOAT,
-    PName CHAR (30)
+    PName CHAR (30),
+    SeID INT,
+    CLID INT,
+    StartTime INT DEFAULT NULL,
+    IID INT,
+    DName INT,
+    PRIMARY KEY (STID),
+    FOREIGN KEY (PName) REFERENCES Program (PName),
+    FOREIGN KEY (SeID) REFERENCES Section (SeID),
+    FOREIGN KEY (CLID) REFERENCES Class (CLID),
+    FOREIGN KEY (StartTime) REFERENCES Timeslot (StartTime),
+    FOREIGN KEY (IID) REFERENCES Instructor (IID),
+    FOREIGN KEY (DName) REFERENCES Department (DName)
 );
 
 INSERT INTO Students
@@ -22,145 +30,149 @@ CREATE TABLE Belong
 (
     STID INT,
     PName CHAR (30),
-    Advisor CHAR (30)
+    Advisor CHAR (30),
+    PRIMARY KEY (STID),
+    PRIMARY KEY (PName)
 );
 
-/* - Undergraduate: redundant but there is so much to add for grad student I didn’t want to combine in Student */
 CREATE TABLE Undergraduate
 (
-    STID INT
+    STID INT,
+    FOREIGN KEY (STID) REFERENCES Student (STID)
 );
 
-/* Graduate: not using null value approach, May need to make a relation to professor i.e SupervisedBy IID instead of an attribute, May need to specify if in thesis based program or course based (can also be done by Null for supervisorID i.e no supervisor because in course based */
 CREATE TABLE Graduate
 (
     STID INT,
-    SupervisorID INT
+    SupervisorID INT,
+    FOREIGN KEY (STID) REFERENCES Student (STID)
 );
 
-/* ResearchFunding: I changed to a weak entity set and added the attribute ammount */
 CREATE TABLE ReseachFunding
 (
     RID INT,
     STID INT,
-    Amount INT
+    Amount INT,
+    PRIMARY KEY (RID),
+    FOREIGN KEY (STID) REFERENCES Graduate (STID)
 );
 
-/* 
-- Teaching assistant: changed to isa from graduate CHAR(1) b/c Y/N whether assigned or not
-- **Changed to Work because can work 2 teaching ass jobs + added CID the course teaching**
-*/
 CREATE TABLE TeachingAssistant
 (
-    CID INT,
+    TAID INT,
     TotalHours INT,
     AssignmentMarking CHAR(1),
     LabInstructor CHAR(1),
-    TutorialSession CHAR(1)
+    NumCourses INT,
+    TutorialSession CHAR(1),
+    PRIMARY KEY (TAID),
+    FOREIGN KEY (STID) REFERENCES Graduate (STID)
 );
 CREATE TABLE Fund
 (
-    CID INT,
-    STID INT
+    TAID INT,
+    RID INT,
+    FOREIGN KEY (TAID) REFERENCES Graduate (STID),
+    FOREIGN KEY (RID) REFERENCES Graduate (STID)
 )
 
-/* Instructor copy pasted attributes ID renamed to IID */
-
-/* Work/teach Many to Many tables with 2 primary keys as seen in class */
 CREATE TABLE Work
 (
-    DID INT,
-    IID INT
+    DName INT,
+    IID INT,
+    FOREIGN KEY (DName) REFERENCES Department (DName),
+    FOREIGN KEY (IID) REFERENCES Instructor (IID)
 );
 CREATE TABLE Teach
 (
-    SID INT,
-    IID INT
+    SeID INT,
+    IID INT,
+    FOREIGN KEY (SeID) REFERENCES Section (SeID),
+    FOREIGN KEY (IID) REFERENCES Instructor (IID)
 );
 
-/* Section ID renamed to SID all attributes from picture */
 CREATE TABLE Section
 (
-    SID INT,
+    SeID INT,
     CLID INT,
-    CID INT,
+    STID INT,
+    PName CHAR (30),
     Semester CHAR (30),
-    Year INT
+    Year INT,
+    PRIMARY KEY (SeID),
+    FOREIGN KEY (PName) REFERENCES Program (PName),
+    FOREIGN KEY (CLID) REFERENCES Class (CLID),
+    FOREIGN KEY (STID) REFERENCES Student (STID)
 );
 
-/* Take Many to Many from class 2 primary keys, grade is 2 char because Letter+ ‘-’or’+’ I.e A-or D+ */
 CREATE TABLE EnrolledIn
 (
     STID INT,
-    SID INT,
-    Grade CHAR (2)
+    SeID INT,
+    Grade CHAR (2),
+    FOREIGN KEY (SeID) REFERENCES Section (SeID),
+    FOREIGN KEY (STID) REFERENCES Student (STID)
 );
 
-/* Class put Number to be key and did the Weak relation */
 CREATE TABLE Class
 (
     CLID INT,
-    Number INT,
+    StartTime INT,
+    ClassNum INT,
     Building CHAR (30),
-    Capacity INT
+    Capacity INT,
+    PRIMARY KEY (CLID),
+    FOREIGN KEY (StartTime) REFERENCES Timeslot (StartTime),
+    FOREIGN KEY (DayWeek) REFERENCES Timeslot (DayWeek)
 );
 
-/*
-- TimeSlot Start time is key and did weak relation 
-- I am unsure about wether or not this should be a weak relation or attribute or what To discuss… 
-*/
 CREATE TABLE Timeslot
 (
     StartTime TIME,
-    Weekday CHAR (30),
+    /* hour minutes seconds */
+    DayWeek CHAR (30),
     CLID INT,
-    Endtime TIME
+    Endtime TIME,
+    PRIMARY KEY (StartTime),
+    PRIMARY KEY (DayWeek),
+    FOREIGN KEY (CLID) REFERENCES Class (CLID)
 );
 
-/* Department copy pasted from picture changed Name to DName */
 CREATE TABLE Department
 (
     DName CHAR (30),
-    Building CHAR (30)
+    IID INT,
+    Building CHAR (30),
+    PRIMARY KEY (DName),
+    FOREIGN KEY (IID) REFERENCES Instructor (IID)
 );
 
-/* Program c/p from pic changed name to PName */
 CREATE TABLE Program
 (
     PName CHAR (30),
-    TotalCredits INT
+    STID INT,
+    TotalCredits INT,
+    PRIMARY KEY (PName),
+    FOREIGN KEY (STID) REFERENCES Student (STID)
 );
 INSERT INTO Program
 VALUES
     ('Computer Science', 72);
 
-/* There were multiple has so I renamed when appropriate to the intials of the two being linke i.e program –has—department renamed to Program—Pdhas—Department */
-CREATE TABLE Under
-(
-    PName CHAR (30),
-    DName CHAR (30)
-);
-
-/* 
-- Note university –has—dep is redundant as there is only 1 university so it is not included
-- Course id changed to CID and name changed to Cname added 1 to1 prerequisite Id attricute 
-*/
 CREATE TABLE Course
 (
     CID INT,
     CName CHAR (30),
-    DName CHAR (30)
+    DName CHAR (30),
+    PRIMARY KEY (CID),
+    FOREIGN KEY (DName) REFERENCES Department (DName)
 );
 CREATE TABLE PrereqFollow
 (
     CID INT,
-    PrereqCID INT
+    FOREIGN KEY (CID) REFERENCES Course (CID)
 );
 
-/* 
-- DChas: DEPARTMENT has course many to many
-- CShas Course has section many to 1 
-*/
 
 
 
