68debb00beb5ef0514ec95e51b0540c10695bc3d
DPigeon <D_Pig@encs.concordia.ca>
2019-07-16 00:41:36 -0400

    Updated Tables with Deniz' Fixes

--- database/sql/tables.sql
+++ database/sql/tables.sql
@@ -3,82 +3,13 @@
 /* Table Student should be fine */
 CREATE TABLE Student
 (
-    STID INT,
-    PName char(30),
-    SeID INT,
+    STID INT PRIMARY KEY,
+    PName char (30) REFERENCES Program (PName),
+    SeID INT REFERENCES Section (SeID),
     Credit INT,
     FirstName CHAR (30),
     LastName CHAR (30),
-    GPA FLOAT,
-    CLID INT,
-    StartTime INT DEFAULT NULL,
-    IID INT,
-    DName INT,
-    PRIMARY KEY(STID)
-);
-
-CREATE TABLE Belong
-(
-    STID INT,
-    PName CHAR (30),
-    Advisor CHAR (30),
-    PRIMARY KEY (STID, PName)
-);
-
-CREATE TABLE Undergraduate
-(
-    STID INT
-);
-
-CREATE TABLE Graduate
-(
-    STID INT,
-    SupervisorID INT
-);
-
-CREATE TABLE ReseachFunding
-(
-    RID INT,
-    STID INT,
-    Amount INT,
-    PRIMARY KEY(RID)
-);
-
-CREATE TABLE TeachingAssistant
-(
-    TAID INT,
-    TotalHours INT,
-    AssignmentMarking CHAR(1),
-    LabInstructor CHAR(1),
-    NumCourses INT,
-    TutorialSession CHAR(1),
-    PRIMARY KEY (TAID)
-);
-CREATE TABLE Fund
-(
-    TAID INT,
-    RID INT,
-    PRIMARY KEY(TAID)
-);
-
-/*CREATE TABLE Work
-(
-    DName INT,
-    IID INT,
-);*/
-
-/*CREATE TABLE Teach
-(
-    SeID INT,
-    IID INT,
-);*/
-
-CREATE TABLE Section
-(
-    SeID INT,
-    Semester CHAR (30),
-    Year INT,
-    PRIMARY KEY (SeID)
+    GPA FLOAT
 );
 
 CREATE TABLE EnrolledIn
@@ -86,143 +17,136 @@ CREATE TABLE EnrolledIn
     STID INT,
     SeID INT,
     Grade CHAR (2),
-    PRIMARY KEY(STID, SeID)
+    FOREIGN KEY (SeID) REFERENCES Section (SeID),
+    FOREIGN KEY (STID) REFERENCES Student (STID)
+);
+
+CREATE TABLE Belong
+(
+    STID INT,
+    PName CHAR (30),
+    Advisor CHAR (30),
+    FOREIGN KEY (STID) REFERENCES Student (STID),
+    FOREIGN KEY (PName) REFERENCES Program (PName)
+);
+
+CREATE TABLE Undergraduate
+(
+    STID INT,
+    FOREIGN KEY (STID) REFERENCES Student (STID)
+);
+
+CREATE TABLE Graduate
+(
+    STID INT,
+    SupervisorID INT,
+    FOREIGN KEY (STID) REFERENCES Student (STID)
+);
+
+CREATE TABLE Fund
+(
+    TAID INT,
+    RID INT,
+    FOREIGN KEY (STID) REFERENCES Graduate (STID)
+)
+
+CREATE TABLE ReseachFunding
+(
+    RID INT PRIMARY KEY,
+    Amount INT,
+    FOREIGN KEY (STID) REFERENCES Graduate (STID)
+);
+
+CREATE TABLE TeachingAssistant
+(
+    TAID INT PRIMARY KEY,
+    TotalHours INT,
+    AssignmentMarking CHAR(1),
+    LabInstructor CHAR(1),
+    NumCourses INT,
+    TutorialSession CHAR(1),
+    FOREIGN KEY (STID) REFERENCES Graduate (STID)
+);
+
+CREATE TABLE Section
+(
+    SeID INT PRIMARY KEY,
+    IID INT REFERENCES Instructor (IID),
+    STID INT REFERENCES Student (STID),
+    CLID INT REFERENCES Class (CLID),
+    CID INT REFERENCES Course (CID),
+    Semester CHAR (30),
+    Year INT,
+);
+
+CREATE TABLE Work
+(
+    DName INT,
+    IID INT,
+    FOREIGN KEY (DName) REFERENCES Department (DName),
+    FOREIGN KEY (IID) REFERENCES Instructor (IID)
+);
+CREATE TABLE Teach
+(
+    SeID INT,
+    IID INT,
+    FOREIGN KEY (SeID) REFERENCES Section (SeID),
+    FOREIGN KEY (IID) REFERENCES Instructor (IID)
 );
 
 CREATE TABLE Class
 (
-    CLID INT,
-    //changing Start Time to time
-    StartTime TIME,
+    CLID INT PRIMARY KEY,
+    StartTime INT REFERENCES Timeslot (StartTime),
+    DayWeek INT REFERENCES Timeslot (DayWeek),
     ClassNum INT,
     Building CHAR (30),
-    Capacity INT,
-    PRIMARY KEY (CLID)
+    Capacity INT
 );
 
 CREATE TABLE Timeslot
 (
-    StartTime TIME,
-    /* hour minutes seconds */
-    DayWeek CHAR (30),
+    StartTime TIME PRIMARY KEY,
+    DayWeek CHAR (30) PRIMARY KEY,
+    CLID INT REFERENCES Class (CLID),
     CLID INT,
-    Endtime TIME,
-    PRIMARY KEY (StartTime, DayWeek)
-    
+    Endtime TIME
 );
 
 CREATE TABLE Department
 (
-    DName CHAR (30),
-    IID INT,
-    Building CHAR (30),
-    PRIMARY KEY (DName)
+    DName CHAR (30) PRIMARY KEY,
+    IID INT REFERENCES Instructor (IID),
+    Building CHAR (30)
 );
 
 CREATE TABLE Program
 (
-    PName CHAR (30),
-    STID INT,
-    TotalCredits INT,
-    PRIMARY KEY(PName)
+    PName CHAR (30) PRIMARY KEY,
+    DName CHAR (30) REFERENCES Department (DName),
+    STID INT REFERENCES Student (STID),
+    TotalCredits INT
 );
 
 CREATE TABLE Course
 (
-    CID INT,
-    CName CHAR (30),
-    DName CHAR (30),
-    PRIMARY KEY (CID)
+    CID INT PRIMARY KEY,
+    CName CHAR (30) REFERENCES Class (CName),
+    DName CHAR (30) REFERENCES Department (DName)
 );
 
-/*CREATE TABLE PrereqFollow
+CREATE TABLE Has
 (
     CID INT,
-    PRIMARY KEY(CID)
-);*/
-
-
-CREATE TABLE Supervisor
-(
-    SupervisorName VARCHAR(20),
-    SupervisorID INT,
-    PRIMARY KEY(SupervisorID)
+    SeID INT,
+    FOREIGN KEY (CID) REFERENCES Course (CID),
+    FOREIGN KEY (SeID) REFERENCES Section (SeID)
 );
 
-/* ALTER TABLES TO ADD FOREIGN KEY(S) (because tables would then be interdependent
-   and all tables must exist before foreign keys can be chosen) */
+CREATE TABLE Contain
+(
+    SeID INT,
+    CLID INT,
+    FOREIGN KEY (SeID) REFERENCES Section (SeID)
+);
 
--- Student table's foreign key(s)
-ALTER TABLE Student ADD FOREIGN KEY (PName) REFERENCES Program (PName);
-ALTER TABLE Student ADD FOREIGN KEY (SeID) REFERENCES Section (SeID);
-ALTER TABLE Student ADD FOREIGN KEY (CLID) REFERENCES Class (CLID);
-ALTER TABLE Student ADD FOREIGN KEY (StartTime) REFERENCES Timeslot (StartTime);
-ALTER TABLE Student ADD FOREIGN KEY (IID) REFERENCES Instructor (IID);
-ALTER TABLE Student ADD FOREIGN KEY (DName) REFERENCES Department (DName);
-
--- Belong table's foreign key(s)
--- N/A
-
--- Undergraduate table's foreign key(s)
-ALTER TABLE Undergraduate ADD FOREIGN KEY (STID) REFERENCES Student (STID);
-
--- Graduate table's foreign key(s)
-ALTER TABLE Graduate ADD FOREIGN KEY (STID) REFERENCES Student (STID);
-
--- ResearchFunding table's foreign key(s)
-ALTER TABLE ResearchFunding ADD FOREIGN KEY (STID) REFERENCES Graduate (STID);
-
--- TeachingAssistant table's foreign key(s)
-ALTER TABLE TeachingAssistant ADD FOREIGN KEY (STID) REFERENCES Graduate (STID);
-
--- Fund table's foreign key(s)
-ALTER TABLE Fund ADD FOREIGN KEY (TAID) REFERENCES Graduate (STID);
-ALTER TABLE Fund ADD FOREIGN KEY (RID) REFERENCES Graduate (STID);
-
--- Work table's foreign key(s) [THIS SEEMS TO BE AN UNUSED TABLE, SO I'M COMMENTING THE SYNTAX BELOW]
---ALTER TABLE Work ADD FOREIGN KEY (DName) REFERENCES Department (DName);
---ALTER TABLE Work ADD FOREIGN KEY (IID) REFERENCES Instructor (IID);
-
-
--- Teach table's foreign key(s) [THIS SEEMS TO BE AN UNUSED TABLE, SO I'M COMMENTING THE SYNTAX BELOW]
---ALTER TABLE Teach ADD FOREIGN KEY (SeID) REFERENCES Section (SeID),
---ALTER TABLE Teach ADD FOREIGN KEY (IID) REFERENCES Instructor (IID)
-
--- Section table's foreign key(s)
-ALTER TABLE Section ADD FOREIGN KEY (PName) REFERENCES Program (PName);
-ALTER TABLE Section ADD FOREIGN KEY (CLID) REFERENCES Class (CLID);
-ALTER TABLE Section ADD FOREIGN KEY (STID) REFERENCES Student (STID);
-
--- EnrolledIn table's foreign key(s)
-ALTER TABLE EnrolledIn ADD FOREIGN KEY (SeID) REFERENCES Section (SeID);
-ALTER TABLE EnrolledIn ADD FOREIGN KEY (STID) REFERENCES Student (STID);
-
--- Class table's foreign key(s)
-ALTER TABLE Class ADD FOREIGN KEY (StartTime) REFERENCES Timeslot(StartTime);
-ALTER TABLE Class ADD FOREIGN KEY (DayWeek) REFERENCES Timeslot (DayWeek);
-
--- Timeslot table's foreign key(s)
-ALTER TABLE Timeslot ADD FOREIGN KEY (CLID) REFERENCES Class(CLID);
-
--- Department table's foreign key(s)
-ALTER TABLE Department ADD FOREIGN KEY (IID) REFERENCES Instructor (IID);
-
--- Program table's foreign key(s)
-ALTER TABLE Program ADD FOREIGN KEY (STID) REFERENCES Student (STID);
-
--- Course table's foreign key(s)
-ALTER TABLE Course ADD FOREIGN KEY (DName) REFERENCES Department (DName);
-
--- PrereqFollow table's foreign key(s) [THIS SEEMS TO BE AN UNUSED TABLE, SO I'M COMMENTING THE SYNTAX BELOW]
---ALTER TABLE PrereqFollow ADD FOREIGN KEY (CID) REFERENCES Course (CID);
-
--- Supervisor table's foreign key(s)
--- N/A
-
--- INSERTION OF VALUES INTO TABLES
-
--- Value(s) to insert into Student table
-INSERT INTO Student VALUES(566090795, 90, 'Linus', 'Torvards', 4.0, 'ComputerScience');
-
--- Value(s) to insert into Program table
-INSERT INTO Program VALUES('Computer Science', 72);
