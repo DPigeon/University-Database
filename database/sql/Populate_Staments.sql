bd67d22b8f16937b0e333143f7cf98fea247052b
Deniz Akcal <deniz@secondary-debian-os>
2019-07-16 16:14:43 -0400

    A typo that was causing problems was fixed.

--- database/sql/Populate_Staments.sql
+++ database/sql/Populate_Staments.sql
@@ -72,7 +72,7 @@ INSERT INTO Class VALUES(12:45:00, 'Friday',5,13:45:00);
 INSERT INTO Class VALUES(10:30:00, 'Monday',6,12:45:00);
 INSERT INTO Class VALUES(11:00:00, 'Monday',7,12:45:00);
 INSERT INTO Class VALUES(10:45:00, 'Wednesday',8,11:45:00);
-INSERT INTO Class VALUES(11:15:00, 'Thursday,9,13:45:00);
+INSERT INTO Class VALUES(11:15:00, 'Thursday',9,13:45:00);
 INSERT INTO Class VALUES(12:30:00, 'Friday',10,14:45:00);
 
 INSERT INTO Department VALUES('Computer Science', 1, 'A Building');
