1498c7cec49afcdbbd7b6a6f47967845e6e1dee5
DPigeon <D_Pig@encs.concordia.ca>
2019-07-18 20:06:43 -0400

    Added missing tuples

--- database/final warmup report/new insert/krc353_1_Undergraduate.sql
+++ database/final warmup report/new insert/krc353_1_Undergraduate.sql
@@ -1,5 +1,40 @@
-INSERT INTO krc353_1.Undergraduate (STID) VALUES (1);
-INSERT INTO krc353_1.Undergraduate (STID) VALUES (2);
-INSERT INTO krc353_1.Undergraduate (STID) VALUES (3);
-INSERT INTO krc353_1.Undergraduate (STID) VALUES (4);
-INSERT INTO krc353_1.Undergraduate (STID) VALUES (5);
+INSERT INTO krc353_1.Undergraduate
+    (STID)
+VALUES
+    (1);
+INSERT INTO krc353_1.Undergraduate
+    (STID)
+VALUES
+    (2);
+INSERT INTO krc353_1.Undergraduate
+    (STID)
+VALUES
+    (3);
+INSERT INTO krc353_1.Undergraduate
+    (STID)
+VALUES
+    (4);
+INSERT INTO krc353_1.Undergraduate
+    (STID)
+VALUES
+    (5);
+INSERT INTO krc353_1.Undergraduate
+    (STID)
+VALUES
+    (28);
+INSERT INTO krc353_1.Undergraduate
+    (STID)
+VALUES
+    (29);
+INSERT INTO krc353_1.Undergraduate
+    (STID)
+VALUES
+    (30);
+INSERT INTO krc353_1.Undergraduate
+    (STID)
+VALUES
+    (31);
+INSERT INTO krc353_1.Undergraduate
+    (STID)
+VALUES
+    (32);
