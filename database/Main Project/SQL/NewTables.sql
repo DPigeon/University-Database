3fa1d610687f4f10173b3bc8bcb313ec5f31458c
Unknown <yana_cr7@live.com>
2019-08-07 11:45:33 -0400

    updated create tables file

--- database/Main Project/SQL/NewTables.sql
+++ database/Main Project/SQL/NewTables.sql
@@ -1,210 +1,606 @@
-CREATE TABLE Campus
+create schema krc353_1 collate latin1_swedish_ci;
+
+create table Block
 (
-    CampID INT PRIMARY KEY,
-    Name CHAR(30)
+	BName char(30) not null
+		primary key,
+	Address varchar(30) null,
+	NumFloors int null,
+	NumRooms int null
 );
 
-CREATE TABLE Block
+create table Campus
 (
-    Name CHAR(30) PRIMARY KEY,
-    Address VARCHAR(30),
-    NumFloors INT
+	CampID int not null
+		primary key,
+	Name char(30) null
 );
 
-CREATE TABLE Room
+create table BlockCamp
 (
-    RoID INT PRIMARY KEY
+	CampID int null,
+	BName varchar(30) default '' not null
+		primary key,
+	constraint BlockCamp_Block_Name_fk
+		foreign key (BName) references Block (BName),
+	constraint BlockCamp_Campus_CampID_fk
+		foreign key (CampID) references Campus (CampID)
 );
 
-CREATE TABLE DeptCamp
+create table Course
 (
-    DName CHAR(30),
-    Chairman CHAR(30),
-    CampID INT,
-    FOREIGN KEY (DName) REFERENCES Department (DName),
-    FOREIGN KEY (Chairman) REFERENCES Department (Chairman)
-);
-Create TABLE Within
-(
-    CID INT,
-    DName CHAR(30),
-    FOREIGN KEY (CID) REFERENCES Course (CID)
-)
-
-CREATE TABLE BlockCamp
-(
-    Name CHAR(30),
-    CampID INT,
-    FOREIGN KEY (Name) REFERENCES Building (Name),
+	CID int not null
+		primary key,
+	CName char(30) null
 );
 
-CREATE TABLE BlockRoom
+create table Department
 (
-    Name CHAR(30),
-    RoID INT,
-    FOREIGN KEY (Name) REFERENCES Building (Name),
-    FOREIGN KEY (RoID) REFERENCES Room (RoID)
+	DName char(30) not null
+		primary key,
+	Chairman char(30) not null
 );
 
-CREATE TABLE Office
+create table DeptCamp
 (
-    RoID INT PRIMARY KEY,
-    FOREIGN KEY (RoID) REFERENCES Room (RoID)
-    ON DELETE CASCADE ON UPDATE CASCADE
+	CampID int not null,
+	DName varchar(30) default '0' not null
+		primary key,
+	constraint DeptCamp_Campus_CampID_fk
+		foreign key (CampID) references Campus (CampID)
+			on update cascade,
+	constraint DeptCamp_Department_DName_fk
+		foreign key (DName) references Department (DName)
+			on update cascade
 );
 
-CREATE TABLE ConferenceRoom
+create table Facility
 (
-    RoID INT PRIMARY KEY,
-    FOREIGN KEY (RoID) REFERENCES Room (RoID)
-    ON DELETE CASCADE ON UPDATE CASCADE
+	equipment varchar(30) null,
+	facID int default 0 not null
+		primary key
 );
 
-CREATE TABLE Facilities
+create table PTInstructor
 (
-    Equipement CHAR(30)
+	IID int default 0 not null
+		primary key
 );
 
-/* Have to check these two tables (Follow and PreReq) */
-CREATE TABLE Follow
+create table PrereqFollow
 (
-    CID INT
+	CID1 int not null,
+	CID2 int not null,
+	primary key (CID1, CID2),
+	constraint `Prereq-Follow_Course_CID_fk`
+		foreign key (CID1) references Course (CID)
+			on update cascade,
+	constraint `Prereq-Follow_Course_CID_fk_2`
+		foreign key (CID2) references Course (CID)
+			on update cascade
 );
 
-CREATE TABLE PreReq
+create table Program
 (
-    CID INT,
-    PreReq INT
+	PName char(30) not null
+		primary key,
+	TotalCredits int null
 );
 
-CREATE TABLE StDegHist
+create table ResearchFunding
 (
-    DegreeName CHAR(30),
-    OverallAvg FLOAT,
-    InstitutionName CHAR(30),
-    DateReceived DATE
+	RID int not null
+		primary key,
+	Amount int null
 );
 
-CREATE TABLE StHomeAddress
+create table Room
 (
-    City CHAR(30),
-    Province CHAR(30),
-    CivicNumber INT,
-    PostalCode INT
+	RoID int auto_increment
+		primary key,
+	ClassNum int null,
+	Capacity int null
 );
 
-CREATE TABLE GradAwards
+create table BlockRoom
 (
-    AwardName CHAR(30),
-    DateReceived DATE
+	RoID int not null,
+	BName varchar(30) not null,
+	primary key (BName, RoID),
+	constraint BlockRoom_Block_Name_fk
+		foreign key (BName) references Block (BName),
+	constraint BlockRoom_Room_RoID_fk
+		foreign key (RoID) references Room (RoID)
 );
 
-CREATE TABLE GradUniversityDegrees
+create table Classroom
 (
-    UniDegree CHAR(30),
-    UniName CHAR(30)
+	ROID int default 0 not null
+		primary key,
+	constraint Classroom_Room_RoID_fk
+		foreign key (ROID) references Room (RoID)
+			on update cascade
 );
 
-CREATE TABLE GradPublications
+create table ConferenceRoom
 (
-    PubName CHAR(30),
-    PubDate DATE
+	RoID int not null
+		primary key,
+	constraint ConferenceRoom_ibfk_1
+		foreign key (RoID) references Room (RoID)
+			on update cascade on delete cascade
 );
 
-CREATE TABLE GradExperience
+create table Contains
 (
-    JobPosition CHAR(30),
-    TimeHeld FLOAT
+	facID int default 0 not null,
+	RoID int default 0 not null,
+	primary key (facID, RoID),
+	constraint Contains_Facility_facID_fk
+		foreign key (facID) references Facility (facID)
+			on update cascade,
+	constraint Contains_Room_RoID_fk
+		foreign key (RoID) references Room (RoID)
+			on update cascade
 );
 
-CREATE TABLE InsExperience
+create table LabRoom
 (
-    JobPosition CHAR(30),
-    TimeHeld FLOAT
+	RoID int not null
+		primary key,
+	constraint LabRoom_Room_RoID_fk
+		foreign key (RoID) references Room (RoID)
+			on update cascade
 );
 
-CREATE TABLE InsAwards
+create table Office
 (
-    AwardName CHAR(30),
-    DateReceived DATE
+	RoID int not null
+		primary key,
+	constraint Office_ibfk_1
+		foreign key (RoID) references Room (RoID)
+			on update cascade on delete cascade
 );
 
-CREATE TABLE InsPublication
+create table SalaryHist
 (
-    PubName CHAR(30),
-    PubDate CHAR(30)
+	IID int null,
+	Salary float null
 );
 
-CREATE TABLE InsUniversityDegrees
+create table Section
 (
-    UniName CHAR(30),
-    UniDegree CHAR(30)
+	SeID int not null
+		primary key,
+	Semester char(30) null,
+	Year int null
 );
 
-CREATE TABLE AssignHist
+create table ClassTimeslot
 (
-    AssignID INT PRIMARY KEY
+	StartTime time default '00:00:00' not null,
+	DayWeek varchar(30) default '' not null,
+	SeID int default 0 not null,
+	EndTime time null,
+	RoID int not null,
+	primary key (StartTime, DayWeek, SeID),
+	constraint ClassTimeslot_ibfk_2
+		foreign key (SeID) references Section (SeID)
 );
 
-CREATE TABLE Contracts
+create index SeID
+	on ClassTimeslot (SeID);
+
+create table Contracts
 (
-    Course INT PRIMARY KEY,
-    Date DATE,
-    Amount FLOAT
+	CID int not null,
+	Date date null,
+	Amount float null,
+	SeID int null,
+	TAID int not null,
+	ContractID int not null
+		primary key,
+	constraint Contracts_Course_CID_fk
+		foreign key (CID) references Course (CID)
+			on update cascade,
+	constraint Contracts_Section_SeID_fk
+		foreign key (SeID) references Section (SeID)
+			on update cascade
 );
 
-CREATE TABLE SalaryHist
+create index Contracts_TeachingAssistant_TAID_fk
+	on Contracts (TAID);
+
+create table Has
 (
-    Salary FLOAT PRIMARY KEY
+	CID int null,
+	SeID int default 0 not null
+		primary key,
+	constraint Has_ibfk_1
+		foreign key (CID) references Course (CID),
+	constraint Has_ibfk_2
+		foreign key (SeID) references Section (SeID)
 );
 
-CREATE TABLE InsHomeAddress
+create index CID
+	on Has (CID);
+
+create table HasContract
 (
-    City CHAR(30),
-    Province CHAR(30),
-    CivicNumber INT,
-    PostalCode INT
+	IID int default 0 not null,
+	ContractID int default 0 not null,
+	primary key (IID, ContractID),
+	constraint HasContract_Contracts_ContractID_fk
+		foreign key (ContractID) references Contracts (ContractID)
+			on update cascade,
+	constraint HasContract_PTInstructor_IID_fk
+		foreign key (IID) references PTInstructor (IID)
+			on update cascade
 );
 
-/* is good Deniz */
-
-CREATE TABLE Student
+create table Student
 (
-    STID INT,
-    SSN INT,
-    Email CHAR(30),
-    GPA FLOAT,
-    Credit INT,
-    Phone INT,
-    FirstName VARCHAR(30),
-    LastName VARCHAR(30),
-    PRIMARY KEY(STID, SSN)
+	STID int not null
+		primary key,
+	Credit int null,
+	FirstName char(30) null,
+	LastName char(30) null,
+	GPA float null,
+	SSN int not null,
+	Phone varchar(30) null,
+	Email varchar(30) null
 );
 
-CREATE TABLE Graduate
+create table Belong
 (
-    STID INT,
-    SSN INT,
-    Thesis INT DEFAULT 0,
-    PRIMARY KEY(STID, SSN),
-    FOREIGN KEY (STID) REFERENCES Student(STID),
-    FOREIGN KEY (SSN) REFERENCES Student(SSN)
+	STID int not null,
+	PName char(30) default '' not null,
+	Advisor char(30) null,
+	primary key (STID, PName),
+	constraint Belong_Student_STID_fk
+		foreign key (STID) references Student (STID)
+			on update cascade,
+	constraint Belong_ibfk_2
+		foreign key (PName) references Program (PName)
 );
 
-CREATE TABLE Supervisor
+create index PName
+	on Belong (PName);
+
+create table EnrolledIn
 (
-    SupervisorID INT,
-    PRIMARY KEY(SupervisorID)
+	STID int default 0 not null,
+	SeID int default 0 not null,
+	Grade char(2) null,
+	primary key (STID, SeID),
+	constraint EnrolledIn_Student_STID_fk
+		foreign key (STID) references Student (STID)
+			on update cascade,
+	constraint EnrolledIn_ibfk_1
+		foreign key (SeID) references Section (SeID)
 );
 
+create index SeID
+	on EnrolledIn (SeID);
 
-CREATE TABLE Supervises
+create table Graduate
 (
-    SupID INT,
-    STID INT,
-    SSN INT,
-    FOREIGN KEY (SupID) REFERENCES Supervisor(SupID),
-    FOREIGN KEY (STID) REFERENCES Graduate(STID),
-    FOREIGN KEY (SSN) REFERENCES Graduate(SSN)
+	STID int default 0 not null
+		primary key,
+	thesis tinyint(1) null,
+	constraint Graduate_Student_STID_fk
+		foreign key (STID) references Student (STID)
+			on update cascade
+);
+
+create table Fund
+(
+	STID int not null
+		primary key,
+	RID int null,
+	constraint Fund_Graduate_STID_fk
+		foreign key (STID) references Graduate (STID)
+			on update cascade,
+	constraint Fund_ResearchFunding_RID_fk
+		foreign key (RID) references ResearchFunding (RID)
+);
+
+create table GradAwards
+(
+	AwardName char(30) default '' not null,
+	DateReceived date default '0000-00-00' not null,
+	STID int default 0 not null,
+	primary key (AwardName, DateReceived, STID),
+	constraint GradAwards_Graduate_STID_fk
+		foreign key (STID) references Graduate (STID)
+			on update cascade
+);
+
+create table GradExperience
+(
+	JobPosition char(30) default '' not null,
+	STID int default 0 not null,
+	DateStarted date default '0000-00-00' not null,
+	Company varchar(30) default '' not null,
+	primary key (JobPosition, Company, DateStarted, STID),
+	constraint GradExperience_Graduate_STID_fk
+		foreign key (STID) references Graduate (STID)
+			on update cascade
+);
+
+create table GradPublications
+(
+	PubName char(30) default '' not null,
+	PubDate date default '0000-00-00' not null,
+	STID int default 0 not null,
+	primary key (STID, PubName, PubDate),
+	constraint GradPublications_Graduate_STID_fk
+		foreign key (STID) references Graduate (STID)
+			on update cascade
+);
+
+create index GradPublications_Graduate_STID_SSN_fk
+	on GradPublications (STID);
+
+create table GradUniversityDegrees
+(
+	UniDegree char(30) default '' not null,
+	UniName char(30) default '' not null,
+	DateReceived date default '0000-00-00' not null,
+	STID int default 0 not null,
+	primary key (UniDegree, STID, UniName, DateReceived),
+	constraint GradUniversityDegrees_Graduate_STID_fk
+		foreign key (STID) references Graduate (STID)
+			on update cascade
+);
+
+create index GradUniversityDegrees_Graduate_STID_SSN_fk
+	on GradUniversityDegrees (STID);
+
+create table StDegHist
+(
+	DegreeName char(30) not null,
+	OverallAvg float null,
+	InstitutionName char(30) not null,
+	DateReceived date null,
+	STID int not null,
+	primary key (DegreeName, InstitutionName, STID),
+	constraint StDegHist_Student_STID_fk
+		foreign key (STID) references Student (STID)
+			on update cascade
+);
+
+create table StHomeAddress
+(
+	City char(30) null,
+	Province char(30) null,
+	CivicNumber int null,
+	PostalCode varchar(30) null,
+	STID int not null
+		primary key,
+	constraint StHomeAddress_Student_STID_fk
+		foreign key (STID) references Student (STID)
+			on update cascade
+);
+
+create table Supervisor
+(
+	SupID int not null
+		primary key
+);
+
+create table Instructor
+(
+	IID int not null
+		primary key,
+	ISSN int not null,
+	Phone varchar(30) null,
+	FirstName varchar(30) null,
+	SupID int null,
+	Email varchar(30) null,
+	LastName char(30) null,
+	constraint Instructor_Supervisor_SupID_fk
+		foreign key (SupID) references Supervisor (SupID)
+			on update cascade
+);
+
+create table FTInstructor
+(
+	IID int not null
+		primary key,
+	constraint FTInstructor_Instructor_IID_fk
+		foreign key (IID) references Instructor (IID)
+			on update cascade
+);
+
+create table InsAwards
+(
+	AwardName char(30) default '' not null,
+	DateReceived date default '0000-00-00' not null,
+	IID int default 0 not null,
+	primary key (AwardName, DateReceived, IID),
+	constraint InsAwards_Instructor_IID_fk
+		foreign key (IID) references Instructor (IID)
+			on update cascade
+);
+
+create table InsExperience
+(
+	JobPosition varchar(30) default '' not null,
+	DateStarted date default '0000-00-00' not null,
+	Company varchar(30) default '' not null,
+	IID int default 0 not null,
+	primary key (JobPosition, DateStarted, Company, IID),
+	constraint InsExperience_Instructor_IID_fk
+		foreign key (IID) references Instructor (IID)
+			on update cascade
+);
+
+create table InsHomeAddress
+(
+	City char(30) null,
+	Province char(30) null,
+	CivicNumber int null,
+	PostalCode varchar(30) null,
+	IID int not null
+		primary key,
+	constraint InsHomeAddress_Instructor_IID_fk
+		foreign key (IID) references Instructor (IID)
+			on update cascade
+);
+
+create table InsPublications
+(
+	PubName varchar(30) default '' not null,
+	PubDate date default '0000-00-00' not null,
+	IID int default 0 not null,
+	primary key (PubName, PubDate, IID),
+	constraint InsPublications_Instructor_IID_fk
+		foreign key (IID) references Instructor (IID)
+			on update cascade
+);
+
+create table InsUniversityDegrees
+(
+	UniName varchar(30) default '' not null,
+	UniDegree varchar(30) default '' not null,
+	IID int default 0 not null,
+	DateReceived date default '0000-00-00' not null,
+	primary key (UniName, UniDegree, DateReceived, IID),
+	constraint InsUniversityDegrees_Instructor_IID_fk
+		foreign key (IID) references Instructor (IID)
+			on update cascade
+);
+
+create table Supervises
+(
+	SupID int not null,
+	STID int not null,
+	primary key (STID, SupID),
+	constraint Supervises_Graduate_STID_fk
+		foreign key (STID) references Graduate (STID)
+			on update cascade,
+	constraint Supervises_Supervisor_SupID_fk
+		foreign key (SupID) references Supervisor (SupID)
+);
+
+create table Teach
+(
+	SeID int default 0 not null,
+	IID int default 0 not null,
+	primary key (IID, SeID),
+	constraint Teach_Instructor_IID_fk
+		foreign key (IID) references Instructor (IID)
+			on update cascade,
+	constraint Teach_Section_SeID_fk
+		foreign key (SeID) references Section (SeID)
+			on update cascade
+);
+
+create table TeachingAssistant
+(
+	TAID int not null
+		primary key,
+	TotalHours int null,
+	AssignmentMarking char null,
+	LabInstructor char null,
+	NumCourses int null,
+	TutorialSession char null
+);
+
+create table AssignHist
+(
+	AssignID int not null,
+	TAID int default 0 not null,
+	primary key (AssignID, TAID),
+	constraint AssignHist_TeachingAssistant_TAID_fk
+		foreign key (TAID) references TeachingAssistant (TAID)
+			on update cascade
+);
+
+create table AssignTo
+(
+	SeID int default 0 not null,
+	TAID int default 0 not null,
+	primary key (SeID, TAID),
+	constraint AssignTo_ibfk_1
+		foreign key (SeID) references Section (SeID),
+	constraint AssignTo_ibfk_2
+		foreign key (TAID) references TeachingAssistant (TAID)
+);
+
+create index TAID
+	on AssignTo (TAID);
+
+create table InvolvedIn
+(
+	ContractID int default 0 not null,
+	TAID int default 0 not null,
+	primary key (ContractID, TAID),
+	constraint InvolvedIn_Contracts_ContractID_fk
+		foreign key (ContractID) references Contracts (ContractID)
+			on update cascade,
+	constraint InvolvedIn_TeachingAssistant_TAID_fk
+		foreign key (TAID) references TeachingAssistant (TAID)
+			on update cascade
+);
+
+create table IsTA
+(
+	STID int default 0 not null
+		primary key,
+	TAID int null,
+	constraint IsTA_Graduate_STID_fk
+		foreign key (STID) references Graduate (STID)
+			on update cascade,
+	constraint IsTA_TeachingAssistant_TAID_fk
+		foreign key (TAID) references TeachingAssistant (TAID)
+			on update cascade
+);
+
+create table Under
+(
+	DName char(30) null,
+	PName char(30) default '' not null
+		primary key,
+	constraint Under_Department_DName_fk
+		foreign key (DName) references Department (DName)
+			on update cascade,
+	constraint Under_ibfk_2
+		foreign key (PName) references Program (PName)
+);
+
+create table Undergraduate
+(
+	STID int default 0 not null
+		primary key,
+	constraint Undergraduate_Student_STID_fk
+		foreign key (STID) references Student (STID)
+			on update cascade
+);
+
+create table Within
+(
+	CID int default 0 not null
+		primary key,
+	DName char(30) null,
+	constraint Within_Department_DName_fk
+		foreign key (DName) references Department (DName)
+			on update cascade,
+	constraint Within_ibfk_1
+		foreign key (CID) references Course (CID)
+);
+
+create table Work
+(
+	DName varchar(30) not null,
+	IID int default 0 not null,
+	primary key (DName, IID),
+	constraint Work_Department_DName_fk
+		foreign key (DName) references Department (DName)
+			on update cascade,
+	constraint Work_Instructor_IID_fk
+		foreign key (IID) references Instructor (IID)
+			on update cascade
 );
 
